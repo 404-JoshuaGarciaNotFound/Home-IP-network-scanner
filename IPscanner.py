8436e29b44a7b810d607e968cfeec0977c60603f
404-JoshuaGarciaNotFound <75165126+404-JoshuaGarciaNotFound@users.noreply.github.com>
2021-04-04 01:33:24 -0700

    Added comments

--- IPscanner.py
+++ IPscanner.py
@@ -3,17 +3,16 @@
 from ping3 import ping, verbose_ping
 print("Scanning home network for busy IP addresses. Please wait!...")
 
-val1 = '192.168.0.'
-x = 0
-for x in range (1, 100):
-    #convertnumconvertnum = str(x)
-    string = str(x)
-    string2 = val1 + string
+IP_Start = '192.168.0.'
+Range_Start = 1 #change this for starting range
+Range_End = 100 #change this for ending range
+for x in range (Range_Start, Range_End):
+    string = str(x) #Turns the index into a string to concatenate
+    string2 = IP_Start + string #concatenates the IP address from its current index
     val = ping(string2)  # Returns delay in seconds.
-    #print(val)
-    #print("Tested" + string2)
+    #Prints active IP channels and its delay
     if type(val) == float:
         print("Found a active IP address: " + string2)
-
+        print("IP delay: " + str(val) + " seconds")
 print("All Active IP addresses from range 1-99 tested!")
 input("Press enter to exit.")
