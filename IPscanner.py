bb51c115ec7f25ba41037706033eaec9cebd63ce
404-JoshuaGarciaNotFound <75165126+404-JoshuaGarciaNotFound@users.noreply.github.com>
2021-04-04 01:01:07 -0700

    Upload base files

---
+++ IPscanner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from ping3 import ping, verbose_ping
+val1 = '192.168.0.'
+x = 0
+for x in range (1, 100):
+    #convertnumconvertnum = str(x)
+    string = str(x)
+    string2 = val1 + string
+    val = ping(string2)  # Returns delay in seconds.
+    #print(val)
+    #print("Tested" + string2)
+    if type(val) == float:
+        print("Valid ip address " + string2)
+
+print("All Active IP addresses from range 1-99 tested!")
+input("Press enter to exit ;)")
